75794483b62c4a037d4522d5648094e5c162428e
Maximiliano Osorio <maxiosorio@gmail.com>
2021-10-22 11:47:11 -0300

    add: check best practices for JS promises (#7)

---
+++ src/.eslintrc.js
@@ -0,0 +1,8 @@
+ module.exports = {
+     "extends": ["react-app", "plugin:promise/recommended"],
+     "plugins": ['promise'],
+     "rules" : {
+         "indent": ["warn", 4] // A custom style-related rule for example
+         // More custom rules here
+     }
+ }
