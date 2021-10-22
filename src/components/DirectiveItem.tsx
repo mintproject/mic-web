585350adbd7702c813af97afdd231801e69029f7
Maximiliano Osorio <maxiosorio@gmail.com>
2021-10-22 15:31:47 -0300

    Scheduler container and get history of commands (#8)
    * add: schedule a new container and connect it
    
    * add: history
    
    * add: Directive
    
    * fix: must point to external api

---
+++ src/components/DirectiveItem.tsx
@@ -0,0 +1,27 @@
+import { Button} from "@mui/material";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { Directive} from "../types/mat";
+
+interface Props {
+  directive: Directive;
+}
+const DirectiveItem = (props: Props) => {
+
+  const toogle = (directiveId: string | undefined) => {
+    
+    directiveId && console.log(directiveId)
+  }
+  return (
+    <Grid container spacing={0}>
+      <Grid item xs={2} md={8}>
+        <Box key={props.directive.id}>{props.directive.command}</Box>
+      </Grid>
+      <Grid item xs={2} md={4}>
+        <Button onClick={() => toogle(props.directive.id)}>Add directive</Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default DirectiveItem;
