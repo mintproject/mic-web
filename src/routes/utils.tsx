08e1a60e6f92808bd58441eac8fad384866b76ad
Maximilinano Osorio <mosorio@isi.edu>
2021-11-02 12:09:39 -0300

    fix: removing react-error-boundary dependency

--- src/routes/utils.tsx
+++ src/routes/utils.tsx
@@ -1,27 +1,12 @@
 import * as React from 'react'
 import { Route, RouteComponentProps } from 'react-router-dom'
 import type { RouteProps } from 'react-router-dom'
-import {ErrorBoundary} from 'react-error-boundary'
-
 import { useKeycloak } from '@react-keycloak/web'
 
 interface PrivateRouteParams extends RouteProps {
   component:
     | React.ComponentType<RouteComponentProps<any>>
     | React.ComponentType<any>
-}
-
-interface ErrorProps {
-  error: any,
-  resetErrorBoundary: any
-}
-
-function ErrorFallback(props: ErrorProps) {
-  return (
-    <div role="alert">
-      <p>Something went wrong: {props.error.message}</p>
-    </div>
-  )
 }
 
 export function PrivateRoute({
@@ -35,9 +20,7 @@ export function PrivateRoute({
       {...rest}
       render={(props) =>
         keycloak?.authenticated ? (
-          <ErrorBoundary  FallbackComponent={ErrorFallback}>
             <Component {...props} />
-          </ErrorBoundary>
         ) : (
             keycloak.login()
         )
